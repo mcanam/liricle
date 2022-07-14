a94a6e341d3973aa3443ba8d92070a480aafc1a6
Itai Bar-Haim <itai.barhaim@applitools.com>
2022-07-14 14:55:11 +0300

    fixed word index -1 issue

--- src/liricle.js
+++ src/liricle.js
@@ -4,39 +4,42 @@ import sync from "./sync.js";
 class Liricle {
     constructor() {
         this._activeLine = null;
-        this._onInit = () => {};
-        this._onSync = () => {};
+        this._onInit = () => { };
+        this._onSync = () => { };
 
         this.data = null;
     }
 
     async init({ text, url }) {
         let lrc = text;
-        
+
         if (url) {
             try {
                 const resp = await fetch(url);
                 const body = await resp.text();
-            
+
                 lrc = body;
             }
-            
+
             catch (error) { throw Error(error) }
         }
-        
+
         this.data = parser(lrc);
         this._onInit(this.data);
     }
 
     sync(time, offset = 0) {
         const index = sync(this.data, time + offset);
-        
+
         if (index == null) return;
         if (index == this._activeLine) return;
-        
+
         let line = this.data.lines[index.lineIndex];
-        const { word } = line.words[index.wordIndex];
-        
+        let word = "";
+        if (index.wordIndex >= 0) {
+            word = line.words[index.wordIndex].text;
+        }
+
         this._activeLine = index;
         this._onSync(index.lineIndex, line.text, index.wordIndex, word, this.data);
     }
