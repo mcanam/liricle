bf34cd27d05e425c75b0c0a1e00f78e0ce960258
mcanam <mcanam910@gmail.com>
2022-07-14 21:42:19 +0700

    .

--- src/liricle.js
+++ src/liricle.js
@@ -2,62 +2,78 @@ import parser from "./parser.js";
 import sync from "./sync.js";
 
 class Liricle {
-    constructor() {
-        this._activeLine = null;
-        this._onInit = () => { };
-        this._onSync = () => { };
+      constructor() {
+            this._activeLine = null;
+            this._onInit = () => {};
+            this._onSync = () => {};
 
-        this.data = null;
-    }
+            this.data = null;
+      }
 
-    async init({ text, url }) {
-        let lrc = text;
+      /**
+       * 
+       * @param {*} param0 
+       */
+      async init({ text, url }) {
+            let lrc = text;
 
-        if (url) {
-            try {
-                const resp = await fetch(url);
-                const body = await resp.text();
+            if (url) {
+                  try {
+                        const resp = await fetch(url);
+                        const body = await resp.text();
 
-                lrc = body;
+                        lrc = body;
+                  } 
+                  
+                  catch (error) { throw Error(error) }
             }
 
-            catch (error) { throw Error(error) }
-        }
+            this.data = parser(lrc);
+            this._onInit(this.data);
+      }
 
-        this.data = parser(lrc);
-        this._onInit(this.data);
-    }
+      /**
+       * 
+       * @param {*} time 
+       * @param {*} offset 
+       */
+      sync(time, offset = 0) {
+            const { line, word } = sync(this.data, time + offset);
 
-    sync(time, offset = 0) {
-        const index = sync(this.data, time + offset);
+            // if not enhanced, event update only occurs if it 
+            // reaches the next lyric instead of updating every second.
+            if (!this.data.enhanced) {
+                  if (line.index == null ||
+                      line.index == this._activeLine
+                  ) return;
 
-        if (index == null) return;
-        if (index == this._activeLine) return;
+                  this._activeLine = line.index;
+                  return this._onSync(line, word);
+            }
 
-        let line = this.data.lines[index.lineIndex];
-        let word = "";
-        if (index.wordIndex >= 0) {
-            word = line.words[index.wordIndex].text;
-        }
+            // simplify, i think it's not good if there are too many parameters.
+            this._onSync(line, word);
+      }
 
-        this._activeLine = index;
-        this._onSync(index.lineIndex, line.text, index.wordIndex, word, this.data);
-    }
+      /**
+       * 
+       * @param {*} event 
+       * @param {*} callback 
+       */
+      on(event, callback) {
+            if (typeof callback != "function") {
+                  throw Error("callback must be a function!");
+            }
 
-    on(event, callback) {
-        if (typeof callback != "function") {
-            throw Error("callback must be a function!");
-        }
-
-        switch (event) {
-            case "init":
-                this._onInit = callback;
-                break;
-            case "sync":
-                this._onSync = callback;
-                break;
-        }
-    }
+            switch (event) {
+                  case "init":
+                        this._onInit = callback;
+                        break;
+                  case "sync":
+                        this._onSync = callback;
+                        break;
+            }
+      }
 }
 
 export default Liricle;
