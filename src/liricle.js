15dccfc37a85634f618b4ae1eb8a77816c142ad7
mcanam <mcanam910@gmail.com>
2022-07-25 22:47:03 +0700

    add better error handling

--- src/liricle.js
+++ src/liricle.js
@@ -1,46 +1,74 @@
 import parser from "./parser.js";
 import sync from "./sync.js";
 
-class Liricle {
-      constructor() {
-            this.activeLine = null;
-            this.activeWord = null;
-            this.onInit = () => {};
-            this.onSync = () => {};
+export default class Liricle {
+      #activeLine = null;
+      #activeWord = null;
+      #onInit = () => {};
+      #onSync = () => {};
 
+      constructor() {
             this.data = null;
       }
 
       /**
        * initialize Liricle
        * @param {Object} options
-       * @param {string} options.text - LRC text
-       * @param {string} options.url - LRC file url
+       * @param {string} options.text - lrc text
+       * @param {string} options.url - lrc file url
        */
-      async init({ text, url }) {
-            let lrc = text;
-
+      async init({ text, url }) {            
             if (url) {
                   try {
                         const resp = await fetch(url);
-                        const body = await resp.text();
 
-                        lrc = body;
+                        if (!resp.ok) {
+                              throw Error(`${resp.status} ${resp.statusText} (${resp.url})`);
+                        }
+
+                        const body = await resp.text();
+                        this.data = parser(body);
                   } 
                   
-                  catch (error) { throw Error(error) }
-            }
+                  catch (error) {
+                        throw Error(`Liricle.init(): ${error.message}`);
+                  }
+            } 
 
-            this.data = parser(lrc);
-            this.onInit(this.data);
+            else if (text) {
+                  this.data = parser(text);
+            } 
+            
+            else {
+                  throw Error(`Liricle.init(): missing argument`);
+            }
+            
+            this.#onInit(this.data);
       }
 
       /**
        * sync lyric with current time
-       * @param {number} time - currrent time from audio player or something in seconds
+       * @param {number} time - currrent time from audio player or something else in seconds
        * @param {number} offset - lyric offset in seconds
+       * @param {boolean} continuous - always emit sync event 
        */
       sync(time, offset = 0, continuous = false) {
+            if (time == undefined) {
+                  throw Error("Liricle.sync(): missing 'time' argument");
+            }
+
+            if (typeof time != "number") {
+                  throw Error("Liricle.sync(): 'time' argument must be a number!");
+            }
+
+            if (typeof offset != "number") {
+                  throw Error("Liricle.sync(): 'offset' argument must be a number!");
+            }
+
+            if (typeof continuous != "boolean") {
+                  throw Error("Liricle.sync(): 'continuous' argument must be a boolean!");
+            }
+
             const { line, word } = sync(this.data, time + offset);
             
             if (line == null && word == null) return;
@@ -48,45 +76,43 @@ class Liricle {
             if (this.data.enhanced && word != null) {
                   if (
                         continuous == false &&
-                        line.index == this.activeLine &&
-                        word.index == this.activeWord
+                        line.index == this.#activeLine &&
+                        word.index == this.#activeWord
                   ) return;
 
-                  this.activeLine = line.index;
-                  this.activeWord = word.index;
+                  this.#activeLine = line.index;
+                  this.#activeWord = word.index;
             }
 
             else {
                   if (
                         continuous == false &&
-                        line.index == this.activeLine
+                        line.index == this.#activeLine
                   ) return;
                   
-                  this.activeLine = line.index;
+                  this.#activeLine = line.index;
             }
 
-            this.onSync(line, word);
+            this.#onSync(line, word);
       }
 
       /**
-       * add event listener
+       * listen to lyricle event
        * @param {string} event - event name
        * @param {function} callback - event callback
        */
       on(event, callback) {
             if (typeof callback != "function") {
-                  throw Error("callback must be a function!");
+                  throw Error("Liricle.on(): 'callback' argument must be a function!");
             }
 
             switch (event) {
                   case "init":
-                        this.onInit = callback;
+                        this.#onInit = callback;
                         break;
                   case "sync":
-                        this.onSync = callback;
+                        this.#onSync = callback;
                         break;
             }
       }
 }
-
-export default Liricle;
